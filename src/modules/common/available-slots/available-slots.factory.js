8b90dd6a13d70eff3978cdc962dc289037c2f02f
Daniel Silva <dniskav@gmail.com>
2015-03-24 10:02:06 -0500

    add some missing files

---
+++ src/modules/common/available-slots/available-slots.factory.js
@@ -0,0 +1,204 @@
+// Factory that stores all the data related to available parking lots
+'use strict';
+
+common.factory('SlotsFactory', function ($http, $q, $log, $timeout) {
+  var factory = {},
+    totalSlots = 10,
+    spotsUrl = 'API/spots',
+    availableSlots = 0;
+
+  factory.spots = [];
+
+  // Factory Init
+  factory.init = function () {
+    factory.drawSlots();
+  }
+
+  factory.drawSlots = function () {
+    this.getData()
+      .then(angular.bind(this, function (data) {
+        for(var ndx in data){
+          data[ndx].plate = factory.normalizePlate(data[ndx].plate);
+        }
+        factory.spots = data;
+        factory.setAvailableSlots();
+      }));    
+  };
+
+  // Available slots
+  factory.setAvailableSlots = function () {
+    var parkingList = factory.spots.length,
+      count = 0;
+
+    for (var i = 0; i < parkingList; i++) {
+      count += (factory.spots[i].active) ? 1 : 0;
+    };
+
+    availableSlots = count;
+  };
+
+  factory.getAvailableSlots = function () {
+    return availableSlots;
+  };
+
+  factory.assignSlot = function (id) {
+    var usr,
+        user = {};
+
+    var assign = function() {
+      factory.editData(id, user)
+        .then(angular.bind(this, function (data) {
+          factory.drawSlots();
+        }),
+          function(err) {
+            alert('save fail!!!'); 
+          }
+        );
+    } ;   
+
+    factory.getData(id)
+      .then(
+        angular.bind(this, function (data) {
+          usr = data;
+          user.active = !usr.active;
+          assign();
+        })
+      );
+  };
+
+  // Format plate of vehicle
+  factory.normalizePlate = function (plate){
+    return plate.match(/[A-Za-z0-9]/gi).join('').toUpperCase();  
+  };
+
+  //Insert new vehicle
+  factory.removeUser = function (id) {
+    factory.removeData(id)
+      .then(function (res) {
+        factory.drawSlots();
+        return res
+      });
+  };
+
+  factory.createUser = function (user) {
+
+    // if (typeof factory.searchPlate(user.plate) === 'object') return;
+    
+    // var newUser = {
+    //   active: false,
+    //   type: user.type.toLowerCase(),
+    //   plate: factory.normalizePlate(user.plate),
+    //   name: user.name
+    // };
+
+    factory.setData(user)
+      .then(
+        function (data) {
+          factory.drawSlots();
+        },
+        function(err) {
+          alert('save fail!!!'); 
+        }
+      );
+  };
+
+  factory.editUser = function (user, data) {
+
+    // if (typeof factory.searchPlate(user.plate) === 'object') return;
+    
+    // var newUser = {
+    //   active: false,
+    //   type: user.type.toLowerCase(),
+    //   plate: factory.normalizePlate(user.plate),
+    //   name: user.name
+    // };
+
+    factory.editData(user, data)
+      .then(
+        function (data) {
+          factory.drawSlots();
+        },
+        function(err) {
+          alert('save fail!!!'); 
+        }
+      );
+  };
+
+  // Search plate
+  factory.searchPlate = function (plate){
+
+    plate = factory.normalizePlate(plate);
+    // Iterator to verify that the user plate is not present in the current array 
+    for(var ndx in factory.spots){
+
+      var DbPlate = factory.normalizePlate(factory.spots[ndx].plate);
+      if (DbPlate === plate) return { position: ndx };
+    };
+    return false;
+  };
+
+  // Http Request to parking users object
+  factory.getData = function (id) {
+    var defer = $q.defer(),
+        url = (id)? spotsUrl + '/' + id : spotsUrl;
+
+    $http.get(url)
+      .success(function(data) {
+        defer.resolve(data);
+      })
+      .error(function() {
+        defer.reject();
+      });
+
+    return defer.promise;
+  };
+
+  factory.setData = function (user) {
+    var defer = $q.defer();
+
+    console.log(user);
+
+    $http.post(spotsUrl, user)
+      .success(function(data) {
+        defer.resolve(data);
+      })
+      .error(function() {
+        defer.reject();
+      });
+
+    return defer.promise;
+  };
+
+  factory.editData = function (user, data) {
+    var defer = $q.defer(),
+        url = spotsUrl + '/' + user;
+
+    $http.put(url, data)
+      .success(function(data) {
+        defer.resolve(data);
+      })
+      .error(function() {
+        defer.reject();
+      });
+
+    return defer.promise;
+  };
+
+  factory.removeData = function (id) {
+    var defer = $q.defer(),
+        url = spotsUrl + '/' + id;
+
+    $http.delete(url)
+      .success(function(data) {
+        defer.resolve(data);
+      })
+      .error(function() {
+        defer.reject();
+      });
+
+    return defer.promise;
+  };
+
+  // return factory object
+  return factory;
+});
---
+++ src/modules/common/parking-slot/parking-slot.controller.js
@@ -0,0 +1,53 @@
+// Parking Slot Controller
+'use strict';
+
+common.controller('parkingSlotCtrl', function (SlotsFactory, $log, $scope, ngDialog) {
+  // Will hold parking spots info
+  this.spots = {},
+  this.auth = true;
+
+  // Initialitation of SlotsFactory
+  SlotsFactory.init();
+
+  // Watch for any change in spots list
+  $scope.$watch(function() {
+    return SlotsFactory.spots;
+  }, angular.bind(this, function (newvalue) {
+    this.spots = newvalue;
+  }));
+
+  // Assign a slot tu user
+  this.assignSlot = function (id) {
+   SlotsFactory.assignSlot(id);
+    this.spots = SlotsFactory.spots;
+  };
+  // Delete user
+  $scope.delete = function(id){
+    var msg = 'This acction is unrecoverable, are you sure????';
+
+    if(confirm(msg)) {
+      if(SlotsFactory.removeUser(id)) {
+        alert(id + ' Deleted!!!');
+      } else {
+        // alert('somenthing happens, cannot delete user :/');
+      };
+    }
+  };
+  // Edit user
+  $scope.edit = function(id){
+    var usr = SlotsFactory.searchPlate(id);
+    $scope.editUsrId = id;
+
+    if(usr.position != '') {
+      // launch the modal window
+      ngDialog.open({ 
+        template: 'views/common/new-vehicle-modal.html',
+        className: 'modal-container',
+        controller: 'addNewModal',
+        scope: $scope  
+      });
+    } else {
+      alert('user not found!');
+    }
+  }
+});
